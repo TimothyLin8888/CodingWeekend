972ddb2c57ce9d6b62b001fa50a47c5cc73050bf
TimothyLin8888 <122694019+TimothyLin8888@users.noreply.github.com>
2024-09-22 14:16:40 -0400

    Just wondering where ProjectLists.txt is because its not the one I have right here

--- ProjectRandomizer/Projects.py
+++ ProjectRandomizer/Projects.py
@@ -10,43 +10,54 @@ def generate_list(projects):
         None
     '''
     try:
-        text_file = open("ProjectLists.txt", "r")
-        projects = [line.strip() for line in text_file]
+        text_file = open("ProjectRandomizer/ProjectLists.txt", "r")
+        for line in text_file:
+            projects.append(line)
     except:
         text_file = open("ProjectLists.txt", "x")
 
-def add(create_list):
-    print("test")
+def add():
+    '''
+    It adds to 'ProjectsLists.txt' so that it is able to be written in without
+    needing to enter the file.
+
+    Parameters:
+        None
+    
+    Returns:
+        None
+    '''
     add_project = input("What do you want to add?\n")
     try:
-        text_file = open("ProjectLists.txt", "a")
-        text_file.write(add_project)
+        text_file = open("ProjectRandomizer/ProjectLists.txt", "a")
+        text_file.write(add_project + "\n")
     except:
         text_file = open("ProjectLists.txt", "x")
 
 def remove(remove_line):
     print("remove")
-    with open("ProjectLists.txt", "r+") as f:
+    with open("ProjectRandomizer/ProjectLists.txt", "r+") as f:
         d = f.readlines()
         f.seek(0)
         for i in d:
-            if i != remove_line:
+            if i != remove_line + "\n":
                 f.write(i)
         f.truncate()
 def main():
     projects = []
+    generate_list(projects)
     while True:
         print("1) Add to the list of projects\n2) Remove from list of projects\n3) Choosing random project\n4) Break the loop")
         num = int(input())
         match num:
             case 1:
                 print(projects)
-                add(projects)
+                add()
             case 2:
                 print(2)
                 remove("Amongus")
             case 3:
-                print(3)
+                print(projects)
             case 4:
                 break
 
