04d45f17419552466d51ae4b73bbd97f5a63a9f4
TimothyLin8888 <122694019+TimothyLin8888@users.noreply.github.com>
2024-09-29 10:44:09 -0400

    Pushing the last few things that I made, could add more later but I'm hungry

--- PersonalFinanceTracker/FinanceTracker.java
+++ PersonalFinanceTracker/FinanceTracker.java
@@ -8,7 +8,7 @@ public class FinanceTracker {
             while (true){
                 System.out.print("What case do you want to do: ");
                 System.out.println("\n1) Add Expense\n2) Remove Expense\n3) View Expense\n4) Total Expense");
-                System.out.println("5) Needs/Wants/Savings Calc\n6) Edit an expense");
+                System.out.println("5) Needs/Wants/Savings Calc\n6) Edit an expense\n7) To break");
                 int choice = scan.nextInt();
                 scan.nextLine();
                 switch (choice) {
@@ -36,7 +36,7 @@ public class FinanceTracker {
                         continue;
                     case 3:
                         for (int i = 0; i < expensesArr.size(); i++){
-                            System.out.println(i + 1 + ")Date: " + expensesArr.get(i).getDate() + " | Description: " + expensesArr.get(i).getDate() + " | Amount: " + expensesArr.get(i).getExpense());
+                            System.out.println(i + 1 + ") Date: " + expensesArr.get(i).getDate() + " | Description: " + expensesArr.get(i).getDescription() + " | Amount: " + expensesArr.get(i).getExpense());
                         }
                         continue;
                     case 4:
@@ -57,30 +57,30 @@ public class FinanceTracker {
                             continue;
                         }
                         for (int i = 0; i < expensesArr.size(); i++){
-                            System.out.println(i + 1 + ") Date: " + expensesArr.get(i).getDate() + " | Description: " + expensesArr.get(i).getDate() + " | Amount: " + expensesArr.get(i).getExpense());
+                            System.out.println(i + 1 + ") Date: " + expensesArr.get(i).getDate() + " | Description: " + expensesArr.get(i).getDescription() + " | Amount: " + expensesArr.get(i).getExpense());
                         }
                         System.out.println("Would you like to edit an Expense (Ep), Description (De), or Date (Da)");
                         String editChoice = scan.nextLine();
-                        if (editChoice.toLowerCase() == "ep") {
+                        if (editChoice.toLowerCase().equals("ep")) {
                             System.out.print("Which expense do you want to edit: ");
                             int num = scan.nextInt();
                             System.out.println("What do you want to change it to: ");
                             double edit = scan.nextDouble();
                             expensesArr.get(num - 1).setExpense(edit);
-                        } else if (editChoice.toLowerCase() == "de"){
+                        } else if (editChoice.toLowerCase().equals("de")){
                             System.out.print("Which description do you want to edit: ");
                             int num = scan.nextInt();
                             System.out.println("What do you want to change it to: ");
                             String edit = scan.nextLine();
                             expensesArr.get(num - 1).setDescription(edit);
-                            scan.nextLine();
-                        } else if (editChoice.toLowerCase() == "da"){
+                        } else if (editChoice.toLowerCase().equals("da")){
                             System.out.print("Which date do you want to edit: ");
                             int num = scan.nextInt();
+                            scan.nextLine();
                             System.out.println("What do you want to change it to: ");
                             String edit = scan.nextLine();
                             expensesArr.get(num - 1).setDate(edit);
-                            scan.nextLine();
+                            System.out.println(expensesArr.get(num -1).getDate());
                         } else {
                             System.out.println("Not one of the options so escape back.");
                         }
