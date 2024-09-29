d99a6a239da2c2c78d949deb44c296cbb3784bdd
TimothyLin8888 <122694019+TimothyLin8888@users.noreply.github.com>
2024-09-29 10:31:44 -0400

    Created way more methods and added more to the FinanceTracker main method

--- PersonalFinanceTracker/FinanceOperations.java
+++ PersonalFinanceTracker/FinanceOperations.java
@@ -1,33 +1,54 @@
+import java.util.ArrayList;
 public class FinanceOperations {
     private double expense;
-    private double income;
-    
-    public FinanceOperations(double expense, double income){
+    private String description;
+    private String date;
+    public FinanceOperations(double expense, String description, String date){
         this.expense = expense;
-        this.income = income;
+        this.description = description;
+        this.date = date;
     }
 
-    public void addExpense(float expense){
-
+    public FinanceOperations(){
+        this.expense = 0.0;
+        this.description = "";
+        this.date = "0000/00/00";
     }
 
-    public double getIncome(){
-        return income;
+    public ArrayList<FinanceOperations> removeExpense(int index, ArrayList<FinanceOperations> expenseArr){
+        expenseArr.remove(index - 1);
+        return expenseArr;
+    }
+
+    public String getDescription(){
+        return description;
     }
 
     public double getExpense(){
         return expense;
     }
 
-    public void setIncome(double income){
-        this.income = income;
+    public String getDate(){
+        return date;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
     }
 
     public void setExpense(double expense){
         this.expense = expense;
     }
 
+    public void setDate(String date){
+        this.date = date;
+    }
+
     public double incomeMinusExpense(double expense, double income){
         return income - expense;
+    }
+
+    public String needWantSave(double income){
+        return "Needs: $" + income * .5 + "\nWants: $" + income * .3 + "\nSavings: $" + income * .2 + "\n";
     }
 }
--- PersonalFinanceTracker/FinanceTracker.java
+++ PersonalFinanceTracker/FinanceTracker.java
@@ -3,23 +3,91 @@ import java.util.*;
 public class FinanceTracker {
     public static void main(String[] args){
         ArrayList<FinanceOperations> expensesArr = new ArrayList<>();
+        FinanceOperations account = new FinanceOperations();
         try (Scanner scan = new Scanner(System.in)) {
             while (true){
                 System.out.print("What case do you want to do: ");
+                System.out.println("\n1) Add Expense\n2) Remove Expense\n3) View Expense\n4) Total Expense");
+                System.out.println("5) Needs/Wants/Savings Calc\n6) Edit an expense");
                 int choice = scan.nextInt();
+                scan.nextLine();
                 switch (choice) {
                     case 1:
-                        System.out.println("case 1");
+                        System.out.println("Choose date: YYYY/MM/DD ");
+                        String date = scan.nextLine();
+                        System.out.println("Type a description about the expense: ");
+                        String description = scan.nextLine();
+                        System.out.println("How much money was it: ");
+                        double expense = scan.nextDouble();
+                        account = new FinanceOperations(expense, description, date);
+                        expensesArr.add(account);
                         continue;
                     case 2:
-                        System.out.println("Case 2");
+                        if (expensesArr.size() == 0){
+                            System.out.println("No expenses available");
+                            continue;
+                        }
+                        System.out.println("Choose expense to remove: \n");
+                        for (int i = 0; i < expensesArr.size(); i++){
+                            System.out.println(i + 1 + ") Date: " + expensesArr.get(i).getDate() + " | Description: " + expensesArr.get(i).getDate() + " | Amount: " + expensesArr.get(i).getExpense());
+                        }
+                        int index = scan.nextInt();
+                        expensesArr = account.removeExpense(index, expensesArr);
                         continue;
                     case 3:
-                        System.out.println("Case 3");
-                        break;
-                    default:
-                        System.out.println("Nice choice");
+                        for (int i = 0; i < expensesArr.size(); i++){
+                            System.out.println(i + 1 + ")Date: " + expensesArr.get(i).getDate() + " | Description: " + expensesArr.get(i).getDate() + " | Amount: " + expensesArr.get(i).getExpense());
+                        }
                         continue;
+                    case 4:
+                        double total = 0;
+                        for (int i = 0; i < expensesArr.size(); i++){
+                            total += expensesArr.get(i).getExpense();
+                        }
+                        System.out.println(total);
+                        continue;
+                    case 5:
+                        System.out.println("Enter your income: ");
+                        double income = scan.nextInt();
+                        System.out.println(account.needWantSave(income));
+                        continue;
+                    case 6:
+                        if (expensesArr.size() == 0) {
+                            System.out.println("Nothing to edit.");
+                            continue;
+                        }
+                        for (int i = 0; i < expensesArr.size(); i++){
+                            System.out.println(i + 1 + ") Date: " + expensesArr.get(i).getDate() + " | Description: " + expensesArr.get(i).getDate() + " | Amount: " + expensesArr.get(i).getExpense());
+                        }
+                        System.out.println("Would you like to edit an Expense (Ep), Description (De), or Date (Da)");
+                        String editChoice = scan.nextLine();
+                        if (editChoice.toLowerCase() == "ep") {
+                            System.out.print("Which expense do you want to edit: ");
+                            int num = scan.nextInt();
+                            System.out.println("What do you want to change it to: ");
+                            double edit = scan.nextDouble();
+                            expensesArr.get(num - 1).setExpense(edit);
+                        } else if (editChoice.toLowerCase() == "de"){
+                            System.out.print("Which description do you want to edit: ");
+                            int num = scan.nextInt();
+                            System.out.println("What do you want to change it to: ");
+                            String edit = scan.nextLine();
+                            expensesArr.get(num - 1).setDescription(edit);
+                            scan.nextLine();
+                        } else if (editChoice.toLowerCase() == "da"){
+                            System.out.print("Which date do you want to edit: ");
+                            int num = scan.nextInt();
+                            System.out.println("What do you want to change it to: ");
+                            String edit = scan.nextLine();
+                            expensesArr.get(num - 1).setDate(edit);
+                            scan.nextLine();
+                        } else {
+                            System.out.println("Not one of the options so escape back.");
+                        }
+                        continue;
+                    default:
+                        System.out.println("Goodbye");
+                        break;
                 }
                 break;
                 /* 
